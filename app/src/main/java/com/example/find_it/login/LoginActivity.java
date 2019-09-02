b8f0d099ce3eb20ac0faa9f6d1b89c072f84df56
ongebo <isaacongebo@gmail.com>
2019-09-02 18:48:45 +0300

    Set login activity as main activity

---
+++ app/src/main/java/com/example/find_it/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.find_it.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.find_it.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
